5410c17c52b23dd925dea89d91ae334dae90c0d3
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-03-16 12:53:03 -0600

    added the ability to use local and production difference

---
+++ local_settibgs.py
@@ -0,0 +1,7 @@
+
+# Date: 16/March/2022 - Wednesday
+# Author: Virgilio Murillo Ochoa
+# personal github: Virgilio-AI
+# linkedin: https://www.linkedin.com/in/virgilio-murillo-ochoa-b29b59203
+# contact: virgiliomurilloochoa1@gmail.com
+DEBUG=True
