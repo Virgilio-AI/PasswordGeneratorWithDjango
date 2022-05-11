15e472e4b62eade71f6f0ca0a141fa30e064f685
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-05-11 11:51:25 -0500

    added steps to deploy on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn passwordGenerator.wsgi --log-file -
--- passwordGenerator/settings.py
+++ passwordGenerator/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
 
 try:
 	from .local_settings import *
